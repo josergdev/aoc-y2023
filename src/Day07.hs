9c3eeaa655f7ba8d1d05964513985764c4e96853
José Rodríguez <joserga@ext.inditex.com>
2023-12-17 14:52:18 +0100

    day 7 part 2

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
 {-# OPTIONS_GHC -Wno-unused-matches #-}
 
-module Day07 where
+module Day07(d07p01, d07p02) where
 
 import qualified Data.List as L
 import qualified Data.Map.Strict as M
@@ -97,4 +98,73 @@ d07p02 = do
   print "Day 07 - Part 2:"
   print $ e2 contents
 
-e2 ss = ss
+newtype OrdCard2 = OrdCard2 Card deriving (Eq)
+
+instance Ord OrdCard2 where
+  compare :: OrdCard2 -> OrdCard2 -> Ordering
+  compare (OrdCard2 CJ) (OrdCard2 CJ) = EQ
+  compare (OrdCard2 CJ) _ = GT
+  compare _ (OrdCard2 CJ) = LT
+  compare (OrdCard2 oc1) (OrdCard2 oc2) = compare oc1 oc2
+
+sortOnCards2 :: [Hand] -> [Hand]
+sortOnCards2 = L.sortOn (O.Down . map OrdCard2 . cards)
+
+kinds2 :: Hand -> [Int]
+kinds2 = M.elems . M.delete CJ . toCountMap
+
+countJ :: Hand -> Int
+countJ = M.findWithDefault 0 CJ . toCountMap
+
+noJoker :: Hand -> Bool
+noJoker = (0 ==) . countJ
+
+isFiveOfAKind2 :: Hand -> Bool
+isFiveOfAKind2 h =
+  isFiveOfAKind h
+    || all ($ h) [(1 ==) . countJ, (1 ==) . count 4 . kinds2]
+    || all ($ h) [(2 ==) . countJ, (1 ==) . count 3 . kinds2]
+    || all ($ h) [(3 ==) . countJ, (1 ==) . count 2 . kinds2]
+    || all ($ h) [(4 ==) . countJ, (1 ==) . count 1 . kinds2]
+
+isFourOfAKind2 :: Hand -> Bool
+isFourOfAKind2 h =
+  all ($ h) [noJoker, isFourOfAKind]
+    || all ($ h) [(1 ==) . countJ, (1 ==) . count 3 . kinds2, (1 ==) . count 1 . kinds2]
+    || all ($ h) [(2 ==) . countJ, (1 ==) . count 2 . kinds2, (1 ==) . count 1 . kinds2]
+    || all ($ h) [(3 ==) . countJ, (2 ==) . count 1 . kinds2]
+
+isFullHouse2 :: Hand -> Bool
+isFullHouse2 h = 
+  all ($ h) [noJoker, isFullHouse]
+    || all ($ h) [(1 ==) . countJ, (2 ==) . count 2 . kinds2]
+
+isThreeOfAKind2 :: Hand -> Bool
+isThreeOfAKind2 h =
+  all ($ h) [noJoker, isThreeOfAKind]
+    || all ($ h) [(1 ==) . countJ, (1 ==) . count 2 . kinds2, (2 ==) . count 1 . kinds2]
+    || all ($ h) [(2 ==) . countJ, (3 ==) . count 1 . kinds2]
+
+isTwoPair2 :: Hand -> Bool
+isTwoPair2 h =
+  all ($ h) [noJoker, isTwoPair]
+
+isOnePair2 :: Hand -> Bool
+isOnePair2 h =
+  all ($ h) [noJoker, isOnePair]
+    || all ($ h) [(1 ==) . countJ, (4 ==) . count 1 . kinds2]
+
+isHighCard2 :: Hand -> Bool
+isHighCard2 h = all ($ h) [(0 ==) . countJ, isHighCard]
+
+groupByType2 :: [Hand] -> [[Hand]]
+groupByType2 hs = map (`filter` hs) [isHighCard2, isOnePair2, isTwoPair2, isThreeOfAKind2, isFullHouse2, isFourOfAKind2, isFiveOfAKind2]
+
+groupsSorted2 :: [Hand] -> [[Hand]]
+groupsSorted2 = map sortOnCards2 . groupByType2
+
+ranked2 :: [Hand] -> Int
+ranked2 = sum . zipWith (*) [1 ..] . map bid . concat . filter (/= []) . groupsSorted2
+
+e2 :: [String] -> Int
+e2 = ranked2 . parseHands
